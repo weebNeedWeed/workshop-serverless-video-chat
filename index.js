1c4f1d128b6c4aed8d3565b5ffa2d05dfaeaabe1
weebNeedWeed <rivenmle@gmail.com>
2024-09-03 04:15:44 +0700

    updated comments for index.js

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
+// Declare variables for use in multiple functions
 let localConnection;
 let localStream;
 let remoteStream;
 let socket;
 
+// Get the roomId in the query string or redirect to the lobby if not provided
 const urlParams = new URLSearchParams(window.location.search);
 const roomId = urlParams.get("room");
-
 if (!roomId) {
   window.location.href = "lobby.html";
 }
 
+// Define the configure for the RTCPeerConnection
 const config = {
   iceServers: [
     {
+      // Here we use the STUN servers owned by Google
       urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
     },
   ],
 };
 
 const init = async () => {
+  // Get user media and display it in the local video element(#user1)
+  // and create a local RTCPeerConnection
   localStream = await navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true,
   });
   document.getElementById("user1").srcObject = localStream;
 
+  // Create a WebSocket connection to the server (API Gateway WebSocket)
   socket = new WebSocket(import.meta.env.VITE_WEBSOCKET_URI);
 
+  // Make a request to the joinRoom route after the connection opened
   socket.onopen = () => {
     socket.send(
       JSON.stringify({
@@ -39,18 +46,22 @@ const init = async () => {
   socket.onmessage = async (event) => {
     const message = JSON.parse(event.data);
 
+    // Create a new Offer and send it to the other participant when he/she newly joins
     if (message.type === "newParticipant") {
       await createOffer();
     }
 
+    // Create a new answer upon receiving offer
     if (message.type === "offer") {
       await createAnswer(message.offer);
     }
 
+    // Accept the answer upon receiving it
     if (message.type === "answer") {
       await acceptAnswer(message.answer);
     }
 
+    // The participant receives new ICE Candidate and adds to the candidate list
     if (message.type === "candidate") {
       await localConnection.addIceCandidate(message.candidate);
     }
@@ -69,6 +80,7 @@ const createAnswer = async (offer) => {
   const answer = await localConnection.createAnswer();
   await localConnection.setLocalDescription(answer);
 
+  // Send the answer to the user who created the offer.
   socket.send(
     JSON.stringify({
       action: "sendMsgToRoom",
@@ -87,6 +99,7 @@ const createOffer = async () => {
   const offer = await localConnection.createOffer();
   await localConnection.setLocalDescription(offer);
 
+  // Send the offer to the other user in the room and wait for the answer
   socket.send(
     JSON.stringify({
       action: "sendMsgToRoom",
@@ -102,19 +115,23 @@ const createOffer = async () => {
 const createConnection = async () => {
   localConnection = new RTCPeerConnection(config);
 
+  // Add the local stream to the connection and send the tracks to the other user
   for (let track of localStream.getTracks()) {
     localConnection.addTrack(track, localStream);
   }
 
+  // Initialize remote stream to display the other user's video
   remoteStream = new MediaStream();
   document.getElementById("user2").srcObject = remoteStream;
 
+  // Receive tracks from the other user, add it to the remote stream
   localConnection.ontrack = (event) => {
     event.streams[0].getTracks().forEach((track) => {
       remoteStream.addTrack(track);
     });
   };
 
+  // If a user has new ICE Candidate, send it to the other user
   localConnection.onicecandidate = (event) => {
     if (event.candidate) {
       setTimeout(() => {
